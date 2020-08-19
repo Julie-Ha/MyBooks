22f0831a6bb065caac692dc21c8cdad14f47cb0c
Julie <julie.hauptmann@wanadoo.fr>
2020-08-19 10:52:51 +0200

    Installation de Webpack et Bootstrap, ajout d'une navbar

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Form\BookType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BooksController extends AbstractController
@@ -12,8 +16,25 @@ class BooksController extends AbstractController
      */
     public function index()
     {
-        return $this->render('books/index.html.twig', [
-            'controller_name' => 'BooksController',
-        ]);
+        return $this->render('books/index.html.twig');
+    }
+
+    /**
+     * @Route("/books/create", name="app_books_create")
+     */
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+    	$book = new Book;
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($book);
+            $em->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('books/create.html.twig',['form'=>$form->createView()]);
     }
 }
